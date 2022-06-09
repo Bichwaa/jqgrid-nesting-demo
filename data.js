b0d741fbd152077753ab85aef7d4a137275c6d17
Bichwaa <ptonny13@gmail.com>
2022-06-09 09:39:55 +0300

    first commit

---
+++ data.js
@@ -0,0 +1,14 @@
+{
+	"rows":[
+		{"CustomerID":"data","CompanyName":"Alfreds Futterkiste","ContactName":"Maria Anders","Phone":"030-0074321","City":"Berlin"},
+		{"CustomerID":"data","CompanyName":"Ana Trujillo Emparedados y helados","ContactName":"Ana Trujillo","Phone":"(5) 555-4729","City":"M\u00e9xico D.F."},
+		{"CustomerID":"data","CompanyName":"Antonio Moreno Taquer\u00eda","ContactName":"Antonio Moreno","Phone":"(5) 555-3932","City":"M\u00e9xico D.F."},
+		{"CustomerID":"data","CompanyName":"Blauer See Delikatessen","ContactName":"Hanna Moos","Phone":"0621-08460","City":"Mannheim"},
+		{"CustomerID":"data","CompanyName":"Blondel p\u00e8re et fils","ContactName":"Fr\u00e9d\u00e9rique Citeaux","Phone":"88.60.15.31","City":"Strasbourg"},
+		{"CustomerID":"data","CompanyName":"Bon app'","ContactName":"Laurence Lebihan","Phone":"91.24.45.40","City":"Marseille"},
+		{"CustomerID":"data","CompanyName":"Bottom-Dollar Markets","ContactName":"Elizabeth Lincoln","Phone":"(604) 555-4729","City":"Tsawassen"},
+		{"CustomerID":"data","CompanyName":"B's Beverages","ContactName":"Victoria Ashworth","Phone":"(171) 555-1212","City":"London"},
+		{"CustomerID":"data","CompanyName":"Cactus Comidas para llevar","ContactName":"Patricio Simpson","Phone":"(1) 135-5555","City":"Buenos Aires"},
+		{"CustomerID":"data","CompanyName":"Chop-suey Chinese","ContactName":"Yang Wang","Phone":"(5) 555-3392","City":"Bern"}
+	]
+}
